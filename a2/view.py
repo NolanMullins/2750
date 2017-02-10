b21114afc7717c65053dc96286b3166e5f41075f
NolanMullins <nolan.mullins@me.com>
2017-02-10 12:15:44 -0500

    added window to view.py

--- a2/view.py
+++ a2/view.py
@@ -1,12 +1,33 @@
 #!/usr/bin/python3
 import sys
+import curses
 
-if (len(sys.argv) <= 1):
+if (len(sys.argv) <= -1):
 	print("No arguments specified")
 	exit(0)
 
 name = ""
 for x in sys.argv[1:]:
 	name+=x+" "
-name = name[:len(name)-1]
-print("'"+name+"'")
+name = name[:-1]
+
+csr = curses.initscr()
+
+csr.addstr(0,0,"ID: "+name)
+
+while (1==1):
+	c = csr.getch(0,0)
+	if (c == ord('q')):
+		break
+	elif (c == ord('o')):
+		csr.addstr(1,0,"O cmd")
+	elif (c == ord('m')):
+		csr.addstr(1,0,"M cmd")
+	elif (c == ord('c')):
+		csr.addstr(1,0,"C cmd")
+	elif (c == ord('s')):
+		csr.addstr(1,0,"S cmd")
+	#refresh page
+	csr.addstr(0,0,"ID: "+name)
+
+curses.endwin()
