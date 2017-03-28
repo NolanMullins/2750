3db8e9b086a7422af61dde003fdfeb1a4956deb6
NolanMullins <nolan.mullins@me.com>
2017-03-28 18:05:32 -0400

    redid stream.c

--- a4/a2/stream.c
+++ a4/a2/stream.c
@@ -5,6 +5,15 @@
 #include "list.h"
 #include "stream.h"
 
+#include <mysql/mysql.h>
+
+#define MAX_QUERY 512
+#define HOSTNAME  "dursley.socs.uoguelph.ca"
+
+#define USERNAME  "mullinsn"
+#define PASSWORD  "0939720"
+#define DATABASE  "mullinsn"
+
 /* appends a char to a string*/
 char* append(char* line, char c)
 {
@@ -22,97 +31,83 @@ char* strgen(char* data)
 	return ptr;
 }
 
+void error(char *msg, MYSQL *mysql){
+	printf("%s\n%s\n",msg,mysql_error(mysql));
+	exit(1);
+}
+
+void createSteamTable(MYSQL* mysql)
+{
+	char query[MAX_QUERY];	
+	*query = 0;
+	
+	strcpy(query, "create table streamData (id int not null auto_increment,");
+	strcat(query, "streamID char(64),");
+	strcat(query, "userID char(64),");
+	strcat(query, "date char(64),");
+	strcat(query, "data varchar(512),");
+	strcat(query, "primary key(id) )");
+	
+	if(mysql_query(mysql, query)){
+	  error("Could not create table",mysql);
+	}
+}
+
+void createUserTable(MYSQL* mysql)
+{
+	char query[MAX_QUERY];
+	*query = 0;
+	
+	strcpy(query, "create table userData (id int not null auto_increment,");
+	strcat(query, "userID char(64),");
+	strcat(query, "streamID char(64),");
+	strcat(query, "postIndex int,");
+	strcat(query, "primary key(id) )");	
+
+	if(mysql_query(mysql, query)){
+	  error("Could not create table",mysql);
+	}
+}
+
+int tableExists(MYSQL* mysql, char* table)
+{
+	MYSQL_RES *res;
+	MYSQL_ROW row;
+	char query[MAX_QUERY];
+	
+	*query = 0;
+
+	strcpy(query, "SELECT count(*) FROM information_schema.tables WHERE table_schema = '");
+	strcat(query, DATABASE);
+	strcat(query, "' AND table_name = '");
+	strcat(query, table);
+	strcat(query, "'");
+
+	if(mysql_query(mysql, query))
+	  error("failed check ",mysql);
+
+	if (!(res = mysql_store_result(mysql))){
+		error("failed store 1",mysql);
+	}
+
+	row = mysql_fetch_row(res);
+	return atoi(row[0]);
+}
+
+void connectToDB(MYSQL* mysql)
+{
+	mysql_init(mysql);
+	mysql_options(mysql, MYSQL_READ_DEFAULT_GROUP, "mydb");
+	if (!mysql_real_connect(mysql, HOSTNAME, USERNAME, PASSWORD,DATABASE, 0, NULL, 0)) 
+	{
+	   error("Could not connect to host.",mysql);
+	}
+}
+
+
 void freeString(void* data)
 {
 	free((char*)data);
-}
-
-int fileNotFound(char* fileName)
-{
-	FILE* f = fopen(fileName, "r");
-	if (f == NULL)
-		return 1;
-	fclose(f);
-	return 0;
-}
-
-int indexOfLastSpace(char* line)
-{
-	int len = strlen(line);
-	int a;
-	for (a=len-1; a>=0; a--)
-		if (line[a] == ' ')
-			return a;
-	return -1;
-}
-
-int cmp(char* name, char* line)
-{
-	/* cmp */
-	int lenN = strlen(name);/*, lenL = strlen(line);*/
-	int sizeOfName = indexOfLastSpace(line);
-	if (lenN == sizeOfName)
-	{
-		int a;
-		for (a = 0; a < lenN; a++)
-		{
-			/*printf("%c:%c ", name[a], line[a]);*/
-			if (name[a] != line[a])
-				return 0;
-		}
-		return 1;
-	}
-	return 0;
-}
-
-int userExists(char* name, char* file)
-{
-	char tmp[256];
-	strcpy(tmp, "messages/");
-	strcat(tmp, file);
-	strcat(tmp, "StreamUsers");
-	FILE* f = fopen(tmp, "r");
-	if (f == NULL)
-		return 0;
-	char line[256];
-	while (fgets(line, 255, f) != NULL)
-	{
-		if (cmp(name, line))
-		{
-			fclose(f);
-			return 1;
-		}
-	}
-	fclose(f);
-	return 0;
-}
-
-void createFile(char* fileName)
-{
-	FILE* f = fopen(fileName, "w");
-	fprintf(f, "tmp");
-	fclose(f);
-}
-
-int sizeOfPost(struct userPost *st)
-{
-	int numBytes = strlen(st->username);
-	numBytes += strlen(st->date);
-	numBytes += strlen(st->text);
-	int a;
-	int size = strlen(st->text);
-	for (a = 0; a < size; a++)
-		if (st->text[a] == '\n' || st->text[a] == '\r')
-			numBytes--;
-	return numBytes;
-}
-
-void printStruct(FILE* stream, FILE* data, struct userPost *st, int end)
-{
-	fprintf(stream, "%s\n", st->username);
-	fprintf(stream, "%s\n", st->date);
-	fprintf(stream, "%s\n", st->text);
-	fprintf(data, "%d\n", end);
 }
 
 /* Lib functions */
@@ -122,60 +117,43 @@ int updateStream(struct userPost *st)
 	if (st->streamname[strlen(st->streamname)-1] == '\n')
 		st->streamname[strlen(st->streamname)-1] = '\0';
 
-	char stream[256];
-	char data[256];
-	strcpy(stream, "messages/");
-	strcat(stream, st->streamname);
-	strcat(stream, "Stream");
-	strcpy(data, "messages/");
-	strcat(data, st->streamname);
-	strcat(data, "StreamData");
+	/* SQL */
+	MYSQL mysql;
+	/*MYSQL_RES *res;
+	MYSQL_ROW row;
+	MYSQL_FIELD *field;*/
 
-	FILE *streamF, *dataF;
-	if (userExists(st->username, st->streamname) == 0)
+	connectToDB(&mysql);
+	if (tableExists(&mysql, "streamData") == 0)
 	{
-		printf("User does not have access to stream: %s\n", st->streamname);
-		return 2;
+		createSteamTable(&mysql);
+		printf("Table was created\n");
 	}
-	if (fileNotFound(stream))
-	{
-		streamF = fopen(stream, "w");
-		dataF = fopen(data, "w");
-		if (streamF == NULL)
-			printf("err: %s\n", strerror(errno));
-		printStruct(streamF, dataF, st,sizeOfPost(st));
-		fclose(streamF);
-		fclose(dataF);
-		printf("Success\n");
-		return 1;
-	}
-	streamF = fopen(stream, "a+");
-	FILE* readBytes = fopen(data, "r");
-	int numBytes = 0;
-	char line[256];
-	/* find number of lines in file */
-	while(fgets(line, 255, readBytes) != NULL) 
-	{
-		if (line[strlen(line)-1] == '\n')
-			line[strlen(line)-1] = '\0';
-		numBytes=atoi(line);
-	}
-	fclose(readBytes);
-	dataF = fopen(data, "a");
-	/*printf("%d\n", numBytes);*/
-	/* find number of lines in this post */
-	int postBytes = sizeOfPost(st);
-	/*printf("postSize: %d\n", postBytes);*/
-	printStruct(streamF, dataF, st, numBytes+postBytes);
-	
-	fclose(streamF);
-	fclose(dataF);
+
+	mysql_close(&mysql);
+	/* end sql */
 	printf("Success\n");
 	return 0;
 }
 
 int addUser(char* username, char* list)
 {
+
+	/* SQL */
+	MYSQL mysql;
+	/*MYSQL_RES *res;
+	MYSQL_ROW row;
+	MYSQL_FIELD *field;*/
+
+	connectToDB(&mysql);
+
+	if (tableExists(&mysql, "userData") == 0)
+	{
+		createUserTable(&mysql);
+		printf("Table was created\n");
+	}
+	/* end sql */
+
 	char stream[256];
 	int a = 0;
 	stream[0] = '\0';
@@ -183,52 +161,12 @@ int addUser(char* username, char* list)
 	{
 		if (list[a] == ',' || list[a] == '\0')
 		{
-			/* list function */
-			char tmp[256];
-			strcpy(tmp, "messages/");
-			strcat(tmp, stream);
-			strcat(tmp, "StreamUsers");
-			FILE* f;
-			if (fileNotFound(tmp))
-			{
-				f = fopen(tmp, "w");
-				if (f == NULL)
-				{
-					printf("err: %s\n", strerror(errno));
-					exit(0);
-				}
-				char streamPath[256];
-				char data[256];
-				strcpy(streamPath, "messages/");
-				strcat(streamPath, stream);
-				strcat(streamPath, "Stream");
-				strcpy(data, "messages/");
-				strcat(data, stream);
-				strcat(data, "StreamData");
-				FILE* streamF = fopen(streamPath, "w");
-				FILE* dataF = fopen(data, "w");
-				if (streamF == NULL)
-				{
-					printf("err: %s\n", strerror(errno));
-					exit(0);
-				}
-				printf("Creating stream: %s\n", stream);
-				fclose(dataF);
-				fclose(streamF);
-			}
-			else
-				f = fopen(tmp, "a");
-			if (userExists(username, stream))
-			{
-				printf("User already in stream: %s\n", stream);
-			}
-			else
-			{
-				fprintf(f, "%s 0\n", username);
-			}
+			/* add user to stream here */
+			*query = 0;
+			sprintf(query, "insert into userData values (null,'%s','%s',0)", username, stream);
+			if(mysql_query(&mysql, records[x]))
+				error("Could not insert record",&mysql);
 			
-			fclose(f);
-			*stream = 0;
 			if (list[a] == '\0')
 				break;
 		}
@@ -236,12 +174,23 @@ int addUser(char* username, char* list)
 			append(stream, list[a]);
 		a++;
 	}
+	mysql_close(&mysql);
 	printf("Success\n");
 	return 0;
 }
 
 int removeUser(char* username, char* list)
 {
+
+	/* SQL */
+	MYSQL mysql;
+	/*MYSQL_RES *res;
+	MYSQL_ROW row;
+	MYSQL_FIELD *field;*/
+
+	connectToDB(&mysql);
+	/* end sql */
+
 	char stream[256];
 	int a = 0;
 	stream[0] = '\0';
@@ -249,45 +198,7 @@ int removeUser(char* username, char* list)
 	{
 		if (list[a] == ',' || list[a] == '\0')
 		{
-			/* list function */
-			char tmp[256];
-			strcpy(tmp, "messages/");
-			strcat(tmp, stream);
-			strcat(tmp, "StreamUsers");
-			FILE* f;
-			if (fileNotFound(tmp))
-			{
-				*stream = 0;
-				if (list[a] == '\0')
-					break;
-				continue;
-			}
-			f = fopen(tmp, "r");
-			/* if the user exists read in the file and remove their line */
-			if (userExists(username, stream))
-			{
-				char line[256];
-				List* lines = init();
-				while (fgets(line, 255, f) != NULL)
-				{
-					if (strlen(line) > 0 && cmp(username, line) == 0)
-					{
-						listAdd(lines, strgen(line));
-					}
-				}
-				fclose(f);
-				f = fopen(tmp, "w");
-				int size = listSize(lines);
-				int b;
-				for (b = 0; b < size; b++)
-					if (((char*)listGet(lines, b))[0] != '\n')
-						fprintf(f, "%s", (char*)listGet(lines, b));
-				lines = listClear(lines, freeString);
-				fclose(f);
-			}
-			else
-				fclose(f);
-			*stream = 0;
+			/* remove user from stream here */
 			if (list[a] == '\0')
 				break;
 		}
@@ -295,6 +206,8 @@ int removeUser(char* username, char* list)
 			append(stream, list[a]);
 		a++;
 	}
+
+	mysql_close(&mysql);
 	printf("Success\n");
 	return 0;
 }
